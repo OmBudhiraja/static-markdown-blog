55e419e1f2852e48ff0c05b29f7ae3e6070eec8d
Om Budhiraja <ombudhiraja144@gmail.com>
2022-01-13 12:27:22 +0530

    added scrollbar to search results

--- components/SearchResults.jsx
+++ components/SearchResults.jsx
@@ -4,7 +4,7 @@ const SearchResults = ({ results }) => {
   if (!results.length) return null;
   return (
     <div className="absolute top-20 right-0 md:right-10 z-10 border-4 border-gray-500 bg-white text-black w-full md:w-6/12 rounded-2xl">
-      <div className="p-10">
+      <div className="p-10 h-[65vh] overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-gray-300 scrollbar-thumb-rounded-full scrollbar-track-rounded-full">
         <h2 className="text-3xl mb-3">{results.length} Results</h2>
         {results.map((result, index) => (
           <Post key={index} post={result} compact={true} />
